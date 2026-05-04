cf578dd76f19ad528525a0f2c37d8264441a668b
manoj0508 <manoj.k0508@gmail.com>
2026-05-04 18:37:35 +0530

    LRU Cache project structure

---
+++ src/main/java/org/manoj/spring/cache/SpringApiCacheApplication.java
@@ -0,0 +1,13 @@
+package org.manoj.spring.cache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringApiCacheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringApiCacheApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/manoj/spring/cache/SpringApiCacheApplicationTests.java
@@ -0,0 +1,13 @@
+package org.manoj.spring.cache;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringApiCacheApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
