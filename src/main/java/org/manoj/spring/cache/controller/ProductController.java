9c646a6c55f4bfb5808c7e30469c0bf776c14c65
manoj0508 <manoj.k0508@gmail.com>
2026-05-06 20:51:00 +0530

    added tomcat server

--- src/main/java/org/manoj/spring/cache/controller/ProductController.java
+++ src/main/java/org/manoj/spring/cache/controller/ProductController.java
@@ -2,6 +2,8 @@ package org.manoj.spring.cache.controller;
 
 import org.manoj.spring.cache.entity.Product;
 import org.manoj.spring.cache.service.ProductService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -9,6 +11,8 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/products")
 public class ProductController {
+
+    private final static Logger logger = LoggerFactory.getLogger(ProductController.class);
 
 
     private final ProductService productService;
@@ -20,15 +24,20 @@ public class ProductController {
     @PostMapping
     public ResponseEntity<Product> createProduct(@RequestBody Product product) {
 
-        Product savedProduct = productService.createProduct(product);
+        logger.info("created new product received with product id {}", product.getProductId());
+         productService.createProduct(product);
 
-        return new ResponseEntity<>(savedProduct, HttpStatus.CREATED);
+        return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
     @GetMapping("/{productId}")
     public ResponseEntity<Product> getProduct(@PathVariable Long productId) {
 
         Product product = productService.getProduct(productId);
+
+        if(null != product){
+            logger.info("product details fetched for id {}", productId);
+        }
 
         return ResponseEntity.ok(product);
     }
